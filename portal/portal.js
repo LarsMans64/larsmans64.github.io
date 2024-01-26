8c6b793c679ec065c274a85b12d7d2ab8f5fb442
LarsMans <l.s.vanemmerik@gmail.com>
2024-01-27 00:52:27 +0100

    colors

--- portal/portal.js
+++ portal/portal.js
@@ -11,14 +11,15 @@ class Portal {
         this.rotation += 0.005
         let camPos = camera.toScreenCoords(this.pos)
         let ctx = area.context
-        ctx.fillStyle = "#ff8800"
+        ctx.fillStyle = "#f67e3c"
         ctx.translate(camPos.x, camPos.y + 5)
         ctx.rotate(this.rotation)
-        ctx.fillRect(this.width * -0.5, this.height * -0.5, this.width, this.height)
+        ctx.roundRect(this.width * -0.5, this.height * -0.5, this.width, this.height, 1000)
+        ctx.fill()
         let gradient = ctx.createLinearGradient(0, this.height * 0.5, 0, this.height * 1.5)
-        gradient.addColorStop(0, "#ffaa44aa")
-        gradient.addColorStop(1, "#ffaa4400")
+        gradient.addColorStop(0, "#ffbb99aa")
+        gradient.addColorStop(1, "#ffbb9900")
         ctx.fillStyle = gradient
-        ctx.fillRect(this.width * -0.5, this.height * 0.5, this.width, this.height)
+        ctx.fillRect(this.width * -0.5 + this.height * 0.4, this.height * 0.5, this.width - this.height * 0.8, this.height, 100)
     }
 }
