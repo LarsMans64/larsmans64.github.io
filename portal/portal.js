3d94df0bc54976b5d35496de651f6c294ed232c4
LarsMans <l.s.vanemmerik@gmail.com>
2024-01-27 01:03:41 +0100

    blue

--- portal/portal.js
+++ portal/portal.js
@@ -1,25 +1,31 @@
 class Portal {
-    constructor(x, y) {
+    constructor(x, y, color, innerShine, outerShine) {
         this.pos = new Vector(x, y)
         this.rotation = 0
         this.height = 15
         this.width = 120
         this.link = 0
+        this.color = color
+        this.innerShine = innerShine
+        this.outerShine = outerShine
     }
 
     draw() {
         this.rotation += 0.005
         let camPos = camera.toScreenCoords(this.pos)
         let ctx = area.context
-        ctx.fillStyle = "#f67e3c"
+        ctx.save()
+        ctx.fillStyle = this.color
+        ctx.beginPath()
         ctx.translate(camPos.x, camPos.y + 5)
         ctx.rotate(this.rotation)
         ctx.roundRect(this.width * -0.5, this.height * -0.5, this.width, this.height, 1000)
         ctx.fill()
         let gradient = ctx.createLinearGradient(0, this.height * 0.5, 0, this.height * 1.5)
-        gradient.addColorStop(0, "#ffbb99aa")
-        gradient.addColorStop(1, "#ffbb9900")
+        gradient.addColorStop(0, this.innerShine)
+        gradient.addColorStop(1, this.outerShine)
         ctx.fillStyle = gradient
         ctx.fillRect(this.width * -0.5 + this.height * 0.4, this.height * 0.5, this.width - this.height * 0.8, this.height, 100)
+        ctx.restore()
     }
 }
--- portal/portal_game.js
+++ portal/portal_game.js
@@ -1,7 +1,8 @@
 let area = new Area()
 let camera = new Camera(0, 0)
 
-let orangePortal = new Portal(200, 200)
+let orangePortal = new Portal(200, 200, "#f67e3c", "#ffbb99aa", "#ffbb9900")
+let bluePortal = new Portal(500, 200, "#2183e5", "#99bbffaa", "#99bbff00")
 
 function init() {
     area.start()
@@ -11,5 +12,6 @@ function update() {
     area.clear()
     area.size()
 
+    bluePortal.draw()
     orangePortal.draw()
 }
