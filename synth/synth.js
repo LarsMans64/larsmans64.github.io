7a6bdef12409f7fb5e4e8278e806db6cecc375cc
LarsMans <l.s.vanemmerik@gmail.com>
2024-05-28 20:35:14 +0200

    sine oscillator

--- synth/synth.js
+++ synth/synth.js
@@ -1,13 +1,21 @@
 function playNote(note) {
     let hertz = 261.625565 * 2 ** (note / 12)
     console.log("note: " + note + " hertz: " + hertz);
-    const synth = new Tone.Synth().toDestination();
-    synth.triggerAttackRelease(hertz, "8n");
+    // const synth = new Tone.Synth().toDestination();
+    // synth.triggerAttackRelease(hertz, "8n");
+
+    const ampEnv = new Tone.AmplitudeEnvelope({
+        attack: 0.01,
+        decay: 0.1,
+        sustain: 0.5,
+        release: 0.4
+    }).toDestination();
+
+    const osc = new Tone.Oscillator(hertz, "sine");
+    osc.partials = [1, .9, .1, .9, .1, .9, .1];
+    osc.connect(ampEnv).start();
+    // oscillator.volume.value = -10;
+    // oscillator.start();
+    // oscillator.stop("+0.5");
+    ampEnv.triggerAttackRelease("8n");
 }
-
-/*
-
-0   12  6
-2   4   
-
-*/
