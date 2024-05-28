0f083545cde6b9f1abecce3f3ba43744adfe5532
LarsMans <l.s.vanemmerik@gmail.com>
2024-05-28 14:34:31 +0200

    MUSIC

--- synth/synth.js
+++ synth/synth.js
@@ -1,3 +1,13 @@
 function playNote(note) {
-    console.log("yepyep" + note);
+    let hertz = 260 * 2 ** (note / 12)
+    console.log("note " + note + " " + hertz);
+    const synth = new Tone.Synth().toDestination();
+    synth.triggerAttackRelease(hertz, "8n");
 }
+
+/*
+
+0   12  6
+2   4   
+
+*/
